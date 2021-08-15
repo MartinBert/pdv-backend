9cc16edcf8efc1d45a77da48517b3014fa2f5ac7
MartinBert <martinbertello7@gmail.com>
2021-08-15 10:02:48 -0300

    update

--- documentacion/Correction.sql
+++ documentacion/Correction.sql
@@ -1,2 +1,2 @@
 INSERT INTO "public"."documentos_comerciales" (id,nombre,codigo_documento,letra,tipo,ticket,presupuesto,iva_cat,activo)
-VALUES (30,false,'PR',4,'P','Presupuesto',false,false,true);
+VALUES (30,'Presupuesto','PR','P',false,false,true,4,true);
