0d3a7818715e9f3dbd21a7b61f2e1207d6ce6bf6
EGL-dev <egdev5340@gmail.com>
2021-10-13 20:57:25 -0300

    update

--- src/main/java/com/prysoft/pdv/models/Brand.java
+++ src/main/java/com/prysoft/pdv/models/Brand.java
@@ -12,8 +12,8 @@ public class Brand implements Serializable {
     private Long id;
     @Column(name = "nombre", unique = true, nullable = false)
     private String nombre;
-    @Column(name = "estado", columnDefinition = "boolean default true")
-    private Boolean estado;
+    //@Column(name = "estado", columnDefinition = "boolean default true")
+    //private Boolean estado;
 
     public Long getId() {
         return id;
@@ -31,20 +31,20 @@ public class Brand implements Serializable {
         this.nombre = nombre;
     }
 
-    public Boolean getEstado() {
-        return estado;
-    }
+    //public Boolean getEstado() {
+       // return estado;
+    //}
 
-    public void setEstado(Boolean estado) {
-        this.estado = estado;
-    }
+    //public void setEstado(Boolean estado) {
+        //this.estado = estado;
+    //}
 
     @Override
     public String toString() {
         final StringBuffer sb = new StringBuffer("Brand{");
         sb.append("id=").append(id);
         sb.append(", nombre='").append(nombre).append('\'');
-        sb.append(", estado=").append(estado);
+        //sb.append(", estado=").append(estado);
         sb.append('}');
         return sb.toString();
     }
