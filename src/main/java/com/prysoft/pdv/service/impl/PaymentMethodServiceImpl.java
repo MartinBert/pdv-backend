f2228eecb58d7d74d6a8897e99a4439f6fafb79f
Ema <egdev5340@gmail.com>
2021-10-14 17:52:24 -0300

    update

--- src/main/java/com/prysoft/pdv/service/impl/PaymentMethodServiceImpl.java
+++ src/main/java/com/prysoft/pdv/service/impl/PaymentMethodServiceImpl.java
@@ -69,12 +69,10 @@ public class PaymentMethodServiceImpl extends FilterService<PaymentMethod> imple
         String hql;
         List<FilterParam> params = new ArrayList<>();
         if (filterParam.getSucursalId() == null) {
-            hql = "WHERE LOWER(c.nombre) LIKE LOWER('" + filterParam.getMedioPagoName() + "%') GROUP BY c.id ORDER BY c.id ASC"+
-                  "AND c.estado IS TRUE";
+            hql = "WHERE LOWER(c.nombre) LIKE LOWER('" + filterParam.getMedioPagoName() + "%') GROUP BY c.id ORDER BY c.id ASC";
         } else {
             hql = "WHERE (c.sucursal.id) = ('" + filterParam.getSucursalId() + "') " +
-                            "AND LOWER(c.nombre) LIKE LOWER('" + filterParam.getMedioPagoName() + "%') GROUP BY c.id ORDER BY c.id ASC" +
-                            "AND c.estado IS TRUE";
+                            "AND LOWER(c.nombre) LIKE LOWER('" + filterParam.getMedioPagoName() + "%') GROUP BY c.id ORDER BY c.id ASC";
         }
         return getPage(hql, filterParam.getPage() - 1, filterParam.getSize(), params);
     }
