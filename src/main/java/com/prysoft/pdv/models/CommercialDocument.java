9c236e8cfb4134fb09e2e11ce117d2f5fb94a961
Martin Bertello <martinbertello7@gmail.com>
2021-11-04 22:12:40 -0300

    update

--- src/main/java/com/prysoft/pdv/models/CommercialDocument.java
+++ src/main/java/com/prysoft/pdv/models/CommercialDocument.java
@@ -20,17 +20,7 @@ public class CommercialDocument implements Serializable {
     private boolean ticket;
     @Column(name = "presupuesto", columnDefinition = "boolean default false")
     private boolean presupuesto;
-    @Column(name="cuentacorriente", columnDefinition = "boolean default false")
-    private boolean cuentacorriente;
     private int ivaCat;
-
-    public boolean isCuentacorriente() {
-        return cuentacorriente;
-    }
-
-    public void setCuentacorriente(boolean cuentacorriente) {
-        this.cuentacorriente = cuentacorriente;
-    }
 
     private boolean activo;
 
@@ -116,7 +106,6 @@ public class CommercialDocument implements Serializable {
                 ", tipo=" + tipo +
                 ", ticket=" + ticket +
                 ", presupuesto=" + presupuesto +
-                ", cuentacorriente=" + cuentacorriente +
                 ", ivaCat=" + ivaCat +
                 ", activo=" + activo +
                 '}';
